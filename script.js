4e256178d4b995f49d08090dbbe07543ee1cd8cb
Svyatoslav334 <sviatoslav.yurkiv-pi2203@kep.nung.edu.ua>
2025-09-07 17:55:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger-menu');
+const openIcon = document.getElementById('openIcon');
+const closeIcon = document.getElementById('closeIcon');
+const navBar = document.querySelector('.nav-bar');
+burger.addEventListener('click', () => {
+    navBar.classList.toggle('active');
+    openIcon.style.display = navBar.classList.contains('active') ? 'none' : 'inline-block';
+    closeIcon.style.display = navBar.classList.contains('active') ? 'inline-block' : 'none';
+});
