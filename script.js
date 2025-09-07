031472512a2cf41a50bfbe9c487efcbf2730f285
Svyatoslav334 <sviatoslav.yurkiv-pi2203@kep.nung.edu.ua>
2025-09-07 17:56:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,30 @@ const burger = document.querySelector('.burger-menu');
 const openIcon = document.getElementById('openIcon');
 const closeIcon = document.getElementById('closeIcon');
 const navBar = document.querySelector('.nav-bar');
+
 burger.addEventListener('click', () => {
     navBar.classList.toggle('active');
     openIcon.style.display = navBar.classList.contains('active') ? 'none' : 'inline-block';
     closeIcon.style.display = navBar.classList.contains('active') ? 'inline-block' : 'none';
 });
+const animateBlocks = (selector) => {
+    const blocks = document.querySelectorAll(selector);
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+    blocks.forEach(block => {
+        observer.observe(block);
+    });
+};
+
+animateBlocks('.product-card');
+animateBlocks('.testimonial-card');
+animateBlocks('.blog-card');
